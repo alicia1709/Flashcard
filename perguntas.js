e85819d6b9262ee0afe28089e32d07a3be6195a0
alicia1709 <r.alicia@escola.pr.gov.br>
2025-07-03 15:37:10 -0300

    javascript e html

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Revolução Francesa',
+    'Qual evento marcou o início da Revolução francesa?',
+    'A queda da Bastilha em 1789'
+)
+
+criaCartao(
+    'Revolução Francesa',
+    'Quais foram os principais nomes da revolução?',
+    'Maximilien Robespierre, Georges Danton, Jean-Paul Marat'
+)
+
+criaCartao(
+    'Revolução Francesa',
+    'Quem fazia parte do 2°Estado antes da revolução?',
+    '2°Estado: nobreza (rei, rainha e demais nobres)'
+)
+
+criaCartao(
+    'Segunda Guerra Mundial',
+    'Quando começou e terminou a Segunda Guerra Mundial?',
+    'Iniciou em 1939 e terminou em 1945'
+)
+
+criaCartao(
+    'Segunda Guerra Mundial',
+    'Qual foi o estopim da Segunda Guerra Mundial?',
+    'A invasão da Polônia pela Alemanha'
+)
+
+criaCartao(
+    'Segunda Guerra Mundial',
+    'Quais os nomes das alianças durante o conflito?',
+    'Eixo e Aliados'
+)
+
+criaCartao(
+    'História do Brasil',
+    'Quando foi proclamada a Independência do Brasil?',
+    'Em 1822'
+)
+
+criaCartao(
+    'História do Brasil',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Marechal Deodoro da Fonseca'
+)
+
+criaCartao(
+    'História do Brasil',
+    'Qual lei marcou o fim da escravidão no Brasil',
+    'A Lei Áurea (1888)'
+)
