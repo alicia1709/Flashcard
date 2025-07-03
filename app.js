340f3978a87557b4811f6da103dccac4cbba3c44
alicia1709 <r.alicia@escola.pr.gov.br>
2025-07-03 15:58:06 -0300

    js

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 function criaCartao(categoria, pergunta, resposta) {
-    console.log(categoria, pergunta, resposta)
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+                   <h3>Revolução francesa</h3> 
+                   <div class="cartao__conteudo__pergunta">
+                    <p>Qual evento marcou o início da Revolução francesa?</p>
+                   </div>
+                   <div class="cartao__conteudo__resposta">
+                   <p>A queda da Bastilha em 1789</p>
+                   </div>
+                </div>
+                `
+                container.appendChild(cartao)
 }
