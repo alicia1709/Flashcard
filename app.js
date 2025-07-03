837e91f946ee950f1667daa9f7f1477151b10e56
alicia1709 <r.alicia@escola.pr.gov.br>
2025-07-03 16:17:08 -0300

    js corrigido

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ function criaCartao(categoria, pergunta, resposta) {
 
     cartao.innerHTML = `
     <div class="cartao__conteudo">
-                   <h3>${categoria}</h3> 
-                   <div class="cartao__conteudo__pergunta">
-                    <p>${pergunta}</p>
-                   </div>
-                   <div class="cartao__conteudo__resposta">
-                   <p>${resposta}</p>
-                   </div>
-                </div>
-                `
-                container.appendChild(cartao)
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
+    container.appendChild(cartao)
 }
